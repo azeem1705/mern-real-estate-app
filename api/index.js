6a9b5879e21486cd78461119c098748aa2627b7c
azeembdnt <127732565+azeembdnt@users.noreply.github.com>
2023-11-09 15:05:50 +0530

    moongoose connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 const app = express();
+
+
+
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.log('MongoDB connection error:', err);
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
