dd1915ff73098845fde81fcba49b7e777010a719
azeembdnt <127732565+azeembdnt@users.noreply.github.com>
2023-11-09 15:17:21 +0530

    added user model

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /[A-Z]/.test(v);
+      },
+      message: props => `${props.value} must contain at least one capital letter`
+    }
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
